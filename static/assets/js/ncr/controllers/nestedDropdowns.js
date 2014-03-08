4e62d0018fc1900f30cfe48b91e5bcf038c0cb1e
ktgilgen <ktgilgen@gmail.com>
2014-03-08 13:34:34 -0800

    Delete nestedDropdowns.js

--- static/assets/js/ncr/controllers/nestedDropdowns.js
+++
@@ -1,9 +0,0 @@
-//With the help of http://stackoverflow.com/questions/17919808/twitter-bootstrap-multiple-dropdowns-on-click-jquery-assistance -->
-
-$(function(){
-  $('.nested-dropdown').click(function(event){
-                              event.stopPropagation();
-                              var dropdown = $(this).children('.dropdown-menu');
-                              dropdown.toggle();
-                              });
-  });
